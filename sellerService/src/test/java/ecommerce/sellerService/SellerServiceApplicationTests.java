5d525c28dea29df980f30e4ee377a2ac526b0eaa
lass9436 <lass9436@gmail.com>
2024-12-28 10:44:44 +0900

    feat: 판매자 서비스 분리

---
+++ sellerService/src/test/java/ecommerce/sellerService/SellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.sellerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
