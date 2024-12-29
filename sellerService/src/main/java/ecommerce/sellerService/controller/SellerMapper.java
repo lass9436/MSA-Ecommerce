73a3e0c52e4ecdf67360a99822f6e2c463f86c46
lass9436 <lass9436@gmail.com>
2024-12-29 22:49:02 +0900

    refactor: 판매자 서비스 SellerMapper 생성자 private 작성

--- sellerService/src/main/java/ecommerce/sellerService/controller/SellerMapper.java
+++ sellerService/src/main/java/ecommerce/sellerService/controller/SellerMapper.java
@@ -3,6 +3,9 @@ package ecommerce.sellerService.controller;
 import ecommerce.sellerService.domain.Seller;
 
 public class SellerMapper {
+
+	private SellerMapper() {}
+
 	public static Seller toSeller(SellerRequest sellerRequest) {
 		return new Seller(
 			sellerRequest.getSellerId(),
